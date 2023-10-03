a90b8036143c67899563112ecaff0d054919ed1b
toris-dev <dbwnghks921@naver.com>
2023-10-03 06:05:03 +0000

    루트페이지 isLogeedIn 검사 후 Redirect

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Redirect } from 'expo-router';
-import { AuthContext } from '../context/AuthContext';
+import React from 'react';
 
 // user로 로그인했으면 /home 으로 Redirect
 function App() {
-  const [isLoggedIn] = AuthContext()
-  return (
-    isLoggedIn? <Redirect href="/login" />: <Redirect href="(tabs)/home" />
-  )
+  const isLoggedIn = AsyncStorage.getItem('isLoggedIn');
+  return isLoggedIn === 'true' ? (
+    <Redirect href="(tabs)/home" />
+  ) : (
+    <Redirect href="/login" />
+  );
 }
 
 export default App;
